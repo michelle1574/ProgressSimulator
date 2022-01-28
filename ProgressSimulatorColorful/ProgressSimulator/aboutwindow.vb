2a6caaea79f3ed86eed64ef0c878bf5478b8ac3a
Michael <70703680+michelle1574@users.noreply.github.com>
2022-01-28 15:57:01 +0100

    Delete ProgressSimulatorColorful directory

--- ProgressSimulatorColorful/ProgressSimulator/aboutwindow.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class aboutwindow
-
-    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
-        Me.Hide()
-        Me.Close()
-    End Sub
-End Class
