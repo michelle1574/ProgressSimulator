1ceff63b8c17834f9f5d39626c56f7e170d01ec7
Michael <70703680+michelle1574@users.noreply.github.com>
2022-02-28 21:05:39 +0100

    A fix that wasn't created for main branch

--- ProgressSimulator/aboutwindow.Designer.vb
+++ ProgressSimulator/aboutwindow.Designer.vb
@@ -32,9 +32,9 @@ Partial Class aboutwindow
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.Location = New System.Drawing.Point(12, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(274, 75)
+        Me.Label1.Size = New System.Drawing.Size(292, 75)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "ProgressBar Simulator v1.4" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Customizer v1.2" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "ActionButtons v1.1"
+        Me.Label1.Text = "ProgressBar Simulator v1.4.1" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Customizer v1.2" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "ActionButtons v1.1"
         '
         'Button1
         '
--- ProgressSimulator/buttonadditionalactions.vb
+++ ProgressSimulator/buttonadditionalactions.vb
@@ -2,33 +2,12 @@
 
     Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
         Me.Hide()
+        Me.Close()
     End Sub
 
     Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
-        Dim answer As DialogResult
-        answer = MsgBox("This will reset everything to defaults. Continue?", vbYesNo + vbExclamation, "Warning")
-        If answer = vbYes Then
-            startbuttonresets.Checked = False
-            stopresets.Checked = False
-            resetstops.Checked = False
-            Me.Hide()
-        Else
-            Exit Sub
-        End If
-    End Sub
-
-    Private Sub buttonadditionalactions_FormClosing(sender As System.Object, e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
-        Dim answer2 As DialogResult
-        answer2 = MsgBox("This will reset everything to defaults. Continue?", vbYesNo + vbExclamation, "Warning")
-        If answer2 = vbYes Then
-            startbuttonresets.Checked = False
-            stopresets.Checked = False
-            resetstops.Checked = False
-            Me.Hide()
-        Else
-            e.Cancel = True
-            Exit Sub
-        End If
+        Me.Hide()
+        Me.Close()
     End Sub
 
     Private Sub buttonadditionalactions_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
